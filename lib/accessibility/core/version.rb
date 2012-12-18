e4879343300f093a0b8a8ee19f21a3c4130dbae6
Mark Rada <marada@uwaterloo.ca>
2012-12-18 10:13:00 -0500

    Bump version for release

--- lib/accessibility/core/version.rb
+++ lib/accessibility/core/version.rb
@@ -1,6 +1,11 @@
+##
+# Namespace for the various `accessibility` projects that are part
+# of AXElements
 module Accessibility
+  ##
+  # Namespace for `accessibility_core` specific classes
   module Core
     # return [String]
-    VERSION = '0.1.0'
+    VERSION = '0.1.1'
   end
 end
