1ce4ab7f35996acf37a19d257d82e51558c15362
Mark Rada <marada@uwaterloo.ca>
2012-12-24 01:28:13 -0500

    Add tests for NSWorkspace and NSProcessInfo maps

---
+++ test/nsprocess_info_test.rb
@@ -0,0 +1,34 @@
+require 'test/helper'
+require 'accessibility/extras'
+
+class NSProcessInfoTest < MiniTest::Unit::TestCase
+
+  def pinfo
+    NSProcessInfo.processInfo
+  end
+
+  def test_os_version_string
+    str = pinfo.operatingSystemVersionString
+    assert_kind_of String, str
+    assert_match /Version 10/, str
+    assert_match /\(Build /, str
+  end
+
+  def test_system_uptime
+    assert_kind_of Float, pinfo.systemUptime
+    assert pinfo.systemUptime > 0
+  end
+
+  def test_p_count
+    assert_equal `sysctl -n hw.ncpu`.chomp.to_i, pinfo.processorCount
+  end
+
+  def test_active_p_count
+    assert_equal `sysctl -n hw.activecpu`.chomp.to_i, pinfo.activeProcessorCount
+  end
+
+  def test_physical_memory
+    assert_equal `sysctl -n hw.memsize`.chomp.to_i, pinfo.physicalMemory
+  end
+
+end
