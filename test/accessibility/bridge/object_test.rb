95cec093ad1bce6bdaacdc975f72b4b1db36d9f2
Mark Rada <marada@uwaterloo.ca>
2012-12-26 14:53:10 -0500

    Enable a macruby specific test on MRI
    Now that MRI also has Object#to_ruby

--- test/accessibility/bridge/object_test.rb
+++ test/accessibility/bridge/object_test.rb
@@ -3,12 +3,12 @@ require 'accessibility/bridge'
 
 class ObjectTest < MiniTest::Unit::TestCase
 
-  if on_macruby?
-    def test_to_ruby
-      assert_same Object, Object.to_ruby
-      assert_same 10, 10.to_ruby
-    end
+  def test_to_ruby
+    assert_same Object, Object.to_ruby
+    assert_same 10, 10.to_ruby
+  end
 
+  if on_macruby?
     def test_to_ax
       assert_same Object, Object.to_ax
       assert_same 10, 10.to_ax
